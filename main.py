da6609d51fd84646a995715346f58bfc9ebbfb0f
eolus87 <niguvaz@gmail.com>
2023-05-20 23:08:06 +0100

    Very first commit with the initial idea.

---
+++ main.py
@@ -0,0 +1,8 @@
+__author__ = "Nicolas Gutierrez"
+
+# Standard libraries
+# Third party libraries
+from pythonping import ping
+# Custom libraries
+
+response = ping('8.8.8.8', count=1, verbose=True)
