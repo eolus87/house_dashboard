53a31fc3e92fe57fa243460e53f89da84a80c350
eolus87 <niguvaz@gmail.com>
2023-06-15 21:10:43 +0100

    Removing the other tabs from the dashboard, the future is just being a front end, instead of doing the queries inside.

--- main_dashboard.py
+++ main_dashboard.py
@@ -29,8 +29,8 @@ ping_data_extractor = PingDataExtractor(configuration["network"], ping_leader)
 init_table = ping_data_extractor.retrieve_ping_stats(DeviceType.PERSONAL_DEVICE, 10)
 
 # Initialization energy
-power_leader = Leader(configuration["energy"], power_function)
-power_leader.start()
+# power_leader = Leader(configuration["energy"], power_function)
+# power_leader.start()
 
 # Dash layout
 app = Dash(__name__, external_stylesheets=external_stylesheets)
@@ -73,36 +73,36 @@ app.layout = html.Div(style={'backgroundColor': '#111111'},
                                                                    id='tbl')
                                           ),
                                         ]),
-                              dcc.Tab(label='Energy',
-                                      style=styles["tab_style"],
-                                      selected_style=styles["tab_selected_style"], children=[
-                                          html.H2(children="Energy [W]",
-                                                  style={'textAlign': 'center',
-                                                         'color': '#FFFFFF'}
-                                                  ),
-                                          dcc.Interval(
-                                              id='interval_refresh_energy',
-                                              interval=1 * 2050,  # in milliseconds
-                                              n_intervals=0
-                                          ),
-                                          dcc.Graph(id='energy_graph'),
-                                        ]),
-                              dcc.Tab(label='Temperature',
-                                      style=styles["tab_style"],
-                                      selected_style=styles["tab_selected_style"],
-                                      children=[]),
-
-                              dcc.Tab(label='Control',
-                                      style=styles["tab_style"],
-                                      selected_style=styles["tab_selected_style"], children=[
-                                          # Button
-                                          html.Button('Printer',
-                                                      id='button-example-1'),
-                                          html.Div(id='output-container-button',
-                                                   children='Enter a value and press submit',
-                                                   style={'color': '#FFFFFF'}
-                                                   )
-                                      ]),
+                              # dcc.Tab(label='Energy',
+                              #         style=styles["tab_style"],
+                              #         selected_style=styles["tab_selected_style"], children=[
+                              #             html.H2(children="Energy [W]",
+                              #                     style={'textAlign': 'center',
+                              #                            'color': '#FFFFFF'}
+                              #                     ),
+                              #             dcc.Interval(
+                              #                 id='interval_refresh_energy',
+                              #                 interval=1 * 2050,  # in milliseconds
+                              #                 n_intervals=0
+                              #             ),
+                              #             dcc.Graph(id='energy_graph'),
+                              #           ]),
+                              # dcc.Tab(label='Temperature',
+                              #         style=styles["tab_style"],
+                              #         selected_style=styles["tab_selected_style"],
+                              #         children=[]),
+                              #
+                              # dcc.Tab(label='Control',
+                              #         style=styles["tab_style"],
+                              #         selected_style=styles["tab_selected_style"], children=[
+                              #             # Button
+                              #             html.Button('Printer',
+                              #                         id='button-example-1'),
+                              #             html.Div(id='output-container-button',
+                              #                      children='Enter a value and press submit',
+                              #                      style={'color': '#FFFFFF'}
+                              #                      )
+                              #         ]),
 
 
                           ])
@@ -133,34 +133,34 @@ def stream_table(value):
     return ping_data_extractor.retrieve_ping_stats(DeviceType.PERSONAL_DEVICE, 10).to_dict('records')
 
 
-@app.callback(
-    Output(component_id='energy_graph', component_property='figure'),
-    Input(component_id='interval_refresh_energy', component_property="n_intervals")
-)
-def stream_fig_energy(value):
-    df = power_leader.target_deque
-    fig = df.plot(template='plotly_dark')
-    fig.update_layout(
-        xaxis_title="Samples [-]",
-        yaxis_title="Energy [W]",
-    )
-    return fig
-
-
-@app.callback(
-    Output(component_id='output-container-button', component_property='children'),
-    [Input(component_id='button-example-1', component_property='n_clicks')],
-    )
-def update_output(n_clicks):
-    if n_clicks is not None:
-        if n_clicks % 2 == 0:
-            switch_off_function("192.168.0.49")
-        else:
-            switch_on_function("192.168.0.49")
-
-    return 'The button has been clicked {} times'.format(
-        n_clicks
-    )
+# @app.callback(
+#     Output(component_id='energy_graph', component_property='figure'),
+#     Input(component_id='interval_refresh_energy', component_property="n_intervals")
+# )
+# def stream_fig_energy(value):
+#     df = power_leader.target_deque
+#     fig = df.plot(template='plotly_dark')
+#     fig.update_layout(
+#         xaxis_title="Samples [-]",
+#         yaxis_title="Energy [W]",
+#     )
+#     return fig
+#
+#
+# @app.callback(
+#     Output(component_id='output-container-button', component_property='children'),
+#     [Input(component_id='button-example-1', component_property='n_clicks')],
+#     )
+# def update_output(n_clicks):
+#     if n_clicks is not None:
+#         if n_clicks % 2 == 0:
+#             switch_off_function("192.168.0.49")
+#         else:
+#             switch_on_function("192.168.0.49")
+#
+#     return 'The button has been clicked {} times'.format(
+#         n_clicks
+#     )
 
 
 app.run_server(host="0.0.0.0", port=8069, dev_tools_ui=True,  # debug=True,
